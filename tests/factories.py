bd071dc1e1b9e42fd9f52a8bfb419725cc00b99c
Scott Dunleavy <sgd6820@stern.nyu.edu>
2024-03-01 02:51:52 +0000

    added Product model

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,23 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Product
 
 
-class YourResourceModelFactory(factory.Factory):
+class ProductFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Product
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    name = factory.Faker("name")
+    description = factory.Faker("sentence")
+    price = factory.Faker("random_number")
+    category = factory.Faker("sentence")
+    available = factory.Faker("boolean")
+    image_url = factory.Faker("image_url")
 
     # Todo: Add your other attributes here...
