bd3ef91323d762920b039f2283412d8110edc68c
= <nml5648@stern.nyu.edu>
2024-03-11 00:52:00 +0000

    Added create product endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,10 +8,12 @@ from unittest import TestCase
 from wsgi import app
 from service.common import status
 from service.models import db, Product
+from .factories import ProductFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
+BASE_URL = "/products"
 
 
 ######################################################################
@@ -55,4 +57,34 @@ class TestYourResourceService(TestCase):
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    # Todo: Add your test cases here...
+    def test_create_product(self):
+        """It should Create a new Product"""
+        test_product = ProductFactory()
+        logging.debug("Test Product: %s", test_product.serialize())
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(new_product["price"], test_product.price)
+        self.assertEqual(new_product["category"], test_product.category)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["image_url"], test_product.image_url)
+
+        # Todo: Uncomment this code when get_products is implemented
+        # Check that the location header was correct
+        # response = self.client.get(location)
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # new_product = response.get_json()
+        # self.assertEqual(new_product["name"], test_product.name)
+        # self.assertEqual(new_product["description"], test_product.description)
+        # self.assertEqual(new_product["price"], test_product.price)
+        # self.assertEqual(new_product["category"], test_product.category)
+        # self.assertEqual(new_product["available"], test_product.available)
+        # self.assertEqual(new_product["image_url"], test_product.image_url)
