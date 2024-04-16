019861204161f64cbe202287063ffb39ee0b98b9
sgd6820 <144186823+sgd6820@users.noreply.github.com>
2024-04-16 10:22:14 -0400

    Update models.py

--- service/models.py
+++ service/models.py
@@ -16,7 +16,7 @@ db = SQLAlchemy()
 class DataValidationError(Exception):
     """Used for an data validation errors when deserializing"""
 
-
+# pylint: disable=too-many-instance-attributes
 class Product(db.Model):
     """
     Class that represents a Product
